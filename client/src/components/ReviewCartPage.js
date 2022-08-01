320ca3b6a63c8e5c7aae7d39c525f44799538909
Matt <m.msklee@gmail.com>
2022-08-01 13:46:24 -0400

    Products page and some Review Cart page

---
+++ client/src/components/ReviewCartPage.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const ReviewCartPage = () => {
+  const [cartItems, setCartItems] = useState();
+
+  useEffect(() => {
+    fetch("");
+  }, []);
+  return <div>ReviewCartPage</div>;
+};
+
+export default ReviewCartPage;
