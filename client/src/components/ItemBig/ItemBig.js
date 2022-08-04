a71c5ef2154b384bc444d184eb077893656dc6d6
Daoping Zheng <dzheng218@gmail.com>
2022-08-04 12:10:59 -0400

    deleted console.log

--- client/src/components/ItemBig/ItemBig.js
+++ client/src/components/ItemBig/ItemBig.js
@@ -1,6 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import styled from "styled-components";
+import { useParams } from "react-router-dom";
 import { ShopContext } from "../ShopContext";
 import YouMayAlsoLike from "../YouMayAlsoLike";
 import SnackbarComponent from "../SnackbarComponent";
@@ -42,71 +41,67 @@ const ItemBig = () => {
 
     // FETCH details about the individual item
     useEffect(() => {
-    setLoadingState("loading");
-    fetch(`/api/item/${id}`)
+        setLoadingState("loading");
+        fetch(`/api/item/${id}`)
         .then((res) => res.json())
         .then((data) => {
-        console.log(data);
         setItem(data.data);
-        if (data.data.numInStock < 1) {
-            setOutOfStock(true);
-            setButtonMessage("NOT AVAILABLE");
-        } else {
-            setOutOfStock(false);
-            setButtonMessage("ADD TO CART");
-        }
+            if (data.data.numInStock < 1) {
+                setOutOfStock(true);
+                setButtonMessage("NOT AVAILABLE");
+            } else {
+                setOutOfStock(false);
+                setButtonMessage("ADD TO CART");
+            }
         }); 
     }, [id]);
     //GET brand name
     useEffect(() => {
-    if (item) {
-        fetch(`/api/get-brand-name/${item.companyId}`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            steBrandName(data.data);
-            setLoadingState("idle");
-        });
-        if (currentUser){
-        //GET item instock number, compare with the number we've put in shopping-cart
-        //If item in stock, add-it-to-cart 
-        //if not, add-to-cart button disabled, and replaced by NOT AVAILABLE sign
-        fetch(`/api/all-items-in-cart/${currentUser._id}`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            data.data.items.forEach((element) => {
-            console.log(element.quantity > item.numInStock);
-            if (
-                element.itemId === item._id &&
-                element.quantity >= item.numInStock
-            ) {
-                setOutOfStock(true);
-                setButtonMessage("NOT AVAILABLE");
-            }
+        if (item) {
+            fetch(`/api/get-brand-name/${item.companyId}`)
+            .then((res) => res.json())
+            .then((data) => {
+                steBrandName(data.data);
+                setLoadingState("idle");
             });
-        })};
-    }
+            if (currentUser){
+            //GET item instock number, compare with the number we've put in shopping-cart
+            //If item in stock, add-it-to-cart 
+            //if not, add-to-cart button disabled, and replaced by NOT AVAILABLE sign
+            fetch(`/api/all-items-in-cart/${currentUser._id}`)
+            .then((res) => res.json())
+            .then((data) => {
+                data.data.items.forEach((element) => {
+                if (
+                    element.itemId === item._id &&
+                    element.quantity >= item.numInStock
+                ) {
+                    setOutOfStock(true);
+                    setButtonMessage("NOT AVAILABLE");
+                }
+                });
+            })};
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [item]);
 
     // POST item to cart, when button is clicked
     const addToCart = (ev) => {
-    ev.preventDefault();
-    if (currentUser){
-    fetch(`/api/add-item-in-cart/`, {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-        user: currentUser._id,
-        item: id,
-        }),
-    })
+        ev.preventDefault();
+        if (currentUser){
+        fetch(`/api/add-item-in-cart/`, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+            user: currentUser._id,
+            item: id,
+            }),
+        })
         .then((res) => res.json())
         .then((data) => {
         data.data.items.forEach((element) => {
-            console.log(element.quantity > item.numInStock);
             if (
             element.itemId === item._id &&
             element.quantity >= item.numInStock
